316d1f7810ccbb4e7077cd7f82088a6773029643
liguangge <1546626493@qq.com>
2016-11-22 19:35:18 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="main">
+      <v-header></v-header>
+       <div class="tab">
+         <div class="tab-item">
+          <a v-link="{ path: '/goods' }">商品</a></div>
+         <div class="tab-item">评论</div>
+         <div class="tab-item">商家</div>
+      </div>
+      <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  import header from './components/header/header.vue'
+  export default{
+    components: {
+      'v-header': header
+    }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  #main 
+    .tab
+      display: flex
+      width: 100%
+      height: 40px
+      line-height: 40px
+      .tab-item
+        flex: 1
+        text-align: center
+</style>
---
+++ src/main.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import App from './App'
+import goods from 'components/goods/goods'
+
+Vue.use(VueRouter)
+
+let app = Vue.extend(App)
+
+let router = new VueRouter()
+
+router.map({
+	'/goods': {
+		component: goods
+	}
+})
+
+router.start(app, '#app')
